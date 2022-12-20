ed66c0beae0c7ea571d8abf0ab4873be8b2d61ed
= <jamiehaberfield15@gmail.com>
2022-12-20 12:44:37 +0000

    Initial commit

---
+++ src/main/java/net/yam/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.yam.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
